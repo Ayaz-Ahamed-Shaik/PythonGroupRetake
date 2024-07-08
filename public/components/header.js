bc5449bd5db0be4926b08bbbd9f166c8af733095
Ayaz-Ahamed-Shaik <ayaz55ahammad@gmail.com>
2024-07-09 01:25:59 +0530

    nav

---
+++ public/components/header.js
@@ -0,0 +1,10 @@
+<header>
+    <h1>CARS 360</h1>
+    <nav>
+        <h3>HOME</h3>
+        <h3>ABOUT US</h3>
+        <h3>SELL CAR</h3>
+        <h3>BUY CAR</h3>
+        <h3>CAR LOAN</h3>
+    </nav>
+</header>
